4ab82df7e0c53464e9301daf5fc41c0eefb6ee0b
M Fadlika <mfadlika@outlook.com>
2025-12-06 14:18:07 +0700

    buat api plan

--- backend/routers/postRouter.js
+++ backend/routers/postRouter.js
@@ -5,4 +5,13 @@ const postRouter = express.Router();
 
 postRouter.get("/", getPosts);
 
+// get post by id
+// postRouter.get("/:id", getPost);
+
+// get posts by user id
+// postRouter.get("/user/:userId", getPostsByUser);
+
+// create a new post
+// postRouter.post("/", createPost);
+
 module.exports = postRouter;
--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -5,4 +5,10 @@ const userRouter = express.Router();
 
 userRouter.get("/", getUsers);
 
+// get user by id
+// userRouter.get("/:id", getUser);
+
+// get user by username
+// userRouter.get("/username/:username", getUserByUsername);
+
 module.exports = userRouter;

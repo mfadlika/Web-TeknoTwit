4395a168f3be83553f70ca2dd5d963a37916ebeb
rizkipr05 <rizki.qq05@gmail.com>
2025-12-12 06:52:40 +0700

    feat: implement getUsers endpoint to retrieve all users from the database

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const { getUsers } = require("../controller/userController");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 const userRouter = express.Router();
 
 userRouter.get("/", getUsers);
+
+// GET all users
+exports.getUsers = async (req, res) => {
+    try {
+      const users = await prisma.user.findMany();
+      res.json(users);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  };
 
 // get user by id
 // userRouter.get("/:id", getUser);

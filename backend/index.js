5db080d2434fb88ad006872fe453a9edc93a94e7
rizkipr05 <rizki.qq05@gmail.com>
2026-01-07 22:16:38 +0700

    feat: update userrouter dan postrouter

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, ".env") });
 const userRouter = require("./routers/userRouter.js");
 const cors = require("cors");
 const postRouter = require("./routers/postRouter.js");

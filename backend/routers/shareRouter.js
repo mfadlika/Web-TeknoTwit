b5422615c2d80c6b276eee297fb9633861f946be
rizkipr05 <rizki.qq05@gmail.com>
2026-01-08 08:42:42 +0700

    fix router share get

--- backend/routers/shareRouter.js
+++ backend/routers/shareRouter.js
@@ -1,17 +1,11 @@
 const express = require("express");
-const {
-  createShare,
-  getSharesByPost,
-} = require("../controller/shareController");
-const auth = require("../middleware/auth");
-
-const shareRouter = express.Router();
-const express = require("express");
 const auth = require("../middleware/auth");
 const {
   sendDirectMessage,
   getDirectMessages,
 } = require("../controller/shareController");
+
+const shareRouter = express.Router();
 
 shareRouter.post("/dm", auth, sendDirectMessage);
 shareRouter.get("/dm", auth, getDirectMessages);

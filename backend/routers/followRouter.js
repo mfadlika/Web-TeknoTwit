84b9c7dae2ac27baed1deb2def2aed190c5c77e8
rizkipr05 <rizki.qq05@gmail.com>
2026-01-08 10:44:48 +0700

    feat: add followrouter

---
+++ backend/routers/followRouter.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const {
+    followUser,
+    unfollowUser,
+    getFollowers,
+    getFollowing,
+} = require("../controller/followController");
+const auth = require("../middleware/auth");
+
+const followrouter = express.Router();
+
+followrouter.post("/follow/:userId", auth, followUser);
+followrouter.post("/unfollow/:userId", auth, unfollowUser);
+followrouter.get("/followers/:userId", getFollowers);
+followrouter.get("/following/:userId", getFollowing);
+
+module.exports = followrouter;
